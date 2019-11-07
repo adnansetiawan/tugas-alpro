0c236e59c0ab6fa88f9faa77e21e9925a01cb419
adnansetiawan <adnansetiawan@gmail.com>
2019-11-07 16:09:38 +0700

    testing create new class

--- tugas-alpro/src/main/java/tugasalpro/App.java
+++ tugas-alpro/src/main/java/tugasalpro/App.java
@@ -12,6 +12,7 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
+        User user = new User("username", "password");
         System.out.println("Hello World!");
     }
 }
