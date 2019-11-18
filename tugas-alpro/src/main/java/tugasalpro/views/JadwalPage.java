d5eeec8f558cf0f498b9c615f7ab130ba39554ce
Oky Rahmanto <oky.rahmanto@gmail.com>
2019-11-18 13:50:01 +0700

    Add function search

--- tugas-alpro/src/main/java/tugasalpro/views/JadwalPage.java
+++ tugas-alpro/src/main/java/tugasalpro/views/JadwalPage.java
@@ -1,35 +1,36 @@
 package tugasalpro.views;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
-import tugasalpro.Gerbong;
-import tugasalpro.managers.WaktuManager;
 import tugasalpro.managers.JadwalManager;
 import tugasalpro.managers.KeretaManager;
 import tugasalpro.managers.KeretaRuteManager;
+import tugasalpro.managers.KotaManager;
+import tugasalpro.managers.WaktuManager;
 import tugasalpro.models.Jadwal;
 import tugasalpro.models.Kereta;
 import tugasalpro.models.KeretaRute;
 
-
-
-public class JadwalPage{
+public class JadwalPage {
     private KeretaManager keretaManager;
+    private KotaManager kotaManager;
     private KeretaRuteManager keretaRuteManager;
     private WaktuManager waktuManager;
     private JadwalManager jadwalManager;
-    private Gerbong gerbong;
     Scanner scanner;
 
-    public JadwalPage(){
+    public JadwalPage() {
         keretaManager = new KeretaManager();
         keretaRuteManager = new KeretaRuteManager();
         jadwalManager = new JadwalManager();
         waktuManager = new WaktuManager();
-        gerbong = new Gerbong();
+        kotaManager = new KotaManager();
+
         scanner = new Scanner(System.in);
     }
 
@@ -46,24 +47,22 @@ public class JadwalPage{
                 menuGenerate();
             } else if (pilihan == 2) {
                 menuTampil();
-            } 
+            }
 
-        } while (pilihan != 99);   
+        } while (pilihan != 99);
     }
-    
-    
 
     public void menuGenerate() {
         String pilihan;
         int lastIndex = -1;
-        System.out.print("Anda Yakin untuk generate Jadwal Kereta: Y/N");
+        System.out.print("Anda Yakin untuk generate Jadwal Kereta: Y/N : ");
         pilihan = scanner.next();
         if (pilihan.equals("Y")) {
             // generate Jadwal
 
             // get today
             Date dateobj = new Date();
-            
+
             // get last generated index;
             lastIndex = jadwalManager.GetAll().size();
             Jadwal jadwal = new Jadwal();
@@ -72,13 +71,14 @@ public class JadwalPage{
             for (int i = 0; i < listKeretaRute.size(); i++) {
                 KeretaRute keretaRute = listKeretaRute.get(i);
                 listKereta = keretaRute.getKeretaTersedia();
-                for (int j=0; j<listKereta.size(); j++) {
+                for (int j = 0; j < listKereta.size(); j++) {
                     lastIndex++;
-                    jadwal.setKodeJadwal("JW"+lastIndex);
+                    jadwal.setKodeJadwal("JW" + lastIndex);
                     jadwal.setKereta(listKereta.get(j));
                     jadwal.setKotaKeberangkatan(keretaRute.getRuteKereta().getKotaAsal());
                     jadwal.setKotaTujuan(keretaRute.getRuteKereta().getKotaTujuan());
-                    jadwal.setKursiKosong(listKereta.get(j).getJmlGBisnis()*10+listKereta.get(j).getJmlGPremium()*20);
+                    jadwal.setKursiKosong(
+                            listKereta.get(j).getJmlGBisnis() * 10 + listKereta.get(j).getJmlGPremium() * 20);
                     jadwal.setTanggalJadwal(dateobj);
                     jadwal.setWaktuBerangkat(waktuManager.GetByKodeWaktu("TM1"));
                     jadwal.setWaktuTiba(waktuManager.GetByKodeWaktu("TM1"));
@@ -86,34 +86,76 @@ public class JadwalPage{
                 }
 
             }
-            
-            
-
 
         }
 
     }
 
-    public void menuTampil(){
+    public void menuTampil() {
         System.out.println("-------------------------------------------------------");
-        System.out.println("No \t Kode Jadwal \t Tanggal \t Waktu Keberangkatan \t Keberangkatan \t Tujuan \t Waktu Tiba \t KAI \t Status");
+        System.out.println(
+                "No \t Kode Jadwal \t Tanggal \t Waktu Keberangkatan \t Keberangkatan \t Tujuan \t Waktu Tiba \t KAI \t Status");
         List<Jadwal> listJadwal = jadwalManager.GetAll();
 
         int i = 0;
-        for(Jadwal jadwal : listJadwal){
+        for (Jadwal jadwal : listJadwal) {
             i++;
-            System.out.print(i+" \t "+jadwal.getKodeJadwal()+" \t "+jadwal.getTanggalJadwal()+"\t\t"
-                +jadwal.getWaktuBerangkat().getWaktu()+"\t"+jadwal.getKotaKeberangkatan().getNamaKota()
-                +"\t"+jadwal.getKotaTujuan().getNamaKota()+"\t"+jadwal.getWaktuTiba().getWaktu()
-                +"\t"+jadwal.getKereta().getKodeKereta());
-            if (jadwal.getKursiKosong()>0) {
-                System.out.println("Tersisa "+jadwal.getKursiKosong()+" Kursi");
+            System.out.print(i + " \t " + jadwal.getKodeJadwal() + " \t " + jadwal.getTanggalJadwal() + "\t\t"
+                    + jadwal.getWaktuBerangkat().getWaktu() + "\t" + jadwal.getKotaKeberangkatan().getNamaKota() + "\t"
+                    + jadwal.getKotaTujuan().getNamaKota() + "\t" + jadwal.getWaktuTiba().getWaktu() + "\t"
+                    + jadwal.getKereta().getKodeKereta());
+            if (jadwal.getKursiKosong() > 0) {
+                System.out.println("Tersisa " + jadwal.getKursiKosong() + " Kursi");
             } else {
                 System.out.println("Full");
             }
-            
+
         }
         System.out.println("-------------------------------------------------------");
+    }
+
+    public void menuSearch() {
+        String kotaKebrangkatan;
+        String kotaTujuan;
+        Date tanggal = null;
+        System.out.println("Cari Jadwal Kereta Api");
+        System.out.print("Keberangkatan : ");
+        kotaKebrangkatan = scanner.next();
+        System.out.print("Tujuan : ");
+        kotaTujuan = scanner.next();
+        System.out.print("Tanggal : ");
+        try {
+            tanggal = new SimpleDateFormat("dd-MM-yyyy").parse(scanner.next());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("-------------------------------------------------------");
+        System.out.println(
+                "No \t Kode Jadwal \t Tanggal \t Waktu Keberangkatan \t Keberangkatan \t Tujuan \t Waktu Tiba \t KAI \t Status");
+        List<Jadwal> listJadwal = jadwalManager.GetAll();
+
+        int i = 0;
+        for (Jadwal jadwal : listJadwal) {
+            if (jadwal.getKotaKeberangkatan().equals(kotaManager.GetByNamaKota(kotaKebrangkatan)) 
+                && jadwal.getKotaTujuan().equals(kotaManager.GetByNamaKota(kotaTujuan))
+                && jadwal.getTanggalJadwal().equals(tanggal)) {
+
+                System.out.print(i + " \t " + jadwal.getKodeJadwal() + " \t " + jadwal.getTanggalJadwal() + "\t\t"
+                    + jadwal.getWaktuBerangkat().getWaktu() + "\t" + jadwal.getKotaKeberangkatan().getNamaKota() + "\t"
+                    + jadwal.getKotaTujuan().getNamaKota() + "\t" + jadwal.getWaktuTiba().getWaktu() + "\t"
+                    + jadwal.getKereta().getKodeKereta());
+                if (jadwal.getKursiKosong() > 0) {
+                    System.out.println("Tersisa " + jadwal.getKursiKosong() + " Kursi");
+                } else {
+                    System.out.println("Full");
+                }
+
+            }
+        }
+        System.out.println("-------------------------------------------------------");
+
+
     }
 
     
