b4998f4e4cc61808bb66410c52d6565788f10abb
aisitisobariah <23519007@std.stei.itb.ac.id>
2019-11-19 19:53:54 +0700

    Update and fixing code KeretaManager.java

--- tugas-alpro/src/main/java/tugasalpro/managers/KeretaManager.java
+++ tugas-alpro/src/main/java/tugasalpro/managers/KeretaManager.java
@@ -9,18 +9,15 @@ import tugasalpro.*;
 public class KeretaManager {
     private Repository<Kereta> repository;
 
-    public KeretaManager()
-    {
+    public KeretaManager() {
        repository = new Repository<Kereta>("Kereta", Kereta[].class);
     }
-    
-    public void Save(Kereta kereta)
-    {
-        repository.add(kereta);  
+
+    public void add(Kereta kereta) {
+        repository.add(kereta); 
     }
 
-    public List<Kereta> GetAll()
-    {
+    public List<Kereta> GetAll() {
         return repository.getAll();
     }
 
@@ -28,43 +25,55 @@ public class KeretaManager {
     {
         List<Kereta> keretas = repository.getAll();
         Optional<Kereta> selectedKereta = keretas.stream().filter(x->x.getKodeKereta().equals(kodeKereta)).findFirst();
-        if(selectedKereta.isPresent())
-        {
+        if(selectedKereta.isPresent()) {
             return selectedKereta.get();
         }
-        return null;
+        else{
+            return null;
+        }
     }
 
-    public void add(Kereta kereta)
-    {
-        repository.add(kereta); 
-    }
     public int getIndexById(String id){
         List<Kereta> listKereta = repository.getAll();
         int index = -1;
-        for(int i=0; i<listKereta.size(); i++){
-            if(listKereta.get(i).getId().equals(id)){
+        for(int i=0; i<listKereta.size(); i++) {
+            if(listKereta.get(i).getId().equals(id)) {
                 index = i;
                 break;
             }            
         }
         return index;
     }
-    public void edit(Kereta kereta)
-    {
-        int indexFound = getIndexById(kereta.getId());
-        if(indexFound != -1)
-        {
+
+    public int getIndexByKodeKereta(String kodeKereta){
+        List<Kereta> listKereta = repository.getAll();
+        int index = -1;
+        for(int i=0; i<listKereta.size(); i++) {
+            if(listKereta.get(i).getKodeKereta().equals(kodeKereta)){
+                index = i;
+                break;
+            }            
+        }
+        return index;
+    }
+
+    public void edit(Kereta kereta) {
+        try{
+            int indexFound = getIndexById(kereta.getId());
             repository.edit(kereta, indexFound);
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public void delete(Kereta kereta)
-    {
-        int indexFound = getIndexById(kereta.getId());
-        if(indexFound != -1)
-        {
+    public void delete(Kereta kereta) {
+        try{
+            int indexFound = getIndexByKodeKereta(kereta.getKodeKereta());
             repository.delete(kereta, indexFound);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
