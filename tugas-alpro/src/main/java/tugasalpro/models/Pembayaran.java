a51259a59b539a9582a2c03f5a9780551e4fb975
adnansetiawan <adnansetiawan@gmail.com>
2019-11-24 17:52:59 +0700

    set format date of getTanggalPembayaran

--- tugas-alpro/src/main/java/tugasalpro/models/Pembayaran.java
+++ tugas-alpro/src/main/java/tugasalpro/models/Pembayaran.java
@@ -1,60 +1,72 @@
 package tugasalpro.models;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class Pembayaran
-{
+public class Pembayaran {
     private String kodePembayaran;
     private String kodeBooking;
     private String kodeKereta;
     private double totalPembayaran;
     private Date tanggalPembayaran;
     private Tiket tiket;
-    public Pembayaran()
-    {
-        
+
+    public Pembayaran() {
+
     }
-    public void createTiket(String kodeTiket, List<Penumpang> penumpang)
-    {
+
+    public void createTiket(String kodeTiket, List<Penumpang> penumpang) {
         tiket = new Tiket(kodeTiket, penumpang);
     }
-    public Tiket getTiket()
-    {
+
+    public Tiket getTiket() {
         return this.tiket;
     }
+
     public void setKodePembayaran(String kodePembayaran) {
         this.kodePembayaran = kodePembayaran;
     }
+
     public void setTanggalPembayaran(Date tanggalPembayaran) {
         this.tanggalPembayaran = tanggalPembayaran;
     }
+
     public void setTotalPembayaran(Double totalPembayaran) {
         this.totalPembayaran = totalPembayaran;
     }
+
     public void setKodeBooking(String kodeBooking) {
         this.kodeBooking = kodeBooking;
     }
-    public void setKodeKereta(String kodeKereta)
-    {
+
+    public void setKodeKereta(String kodeKereta) {
         this.kodeKereta = kodeKereta;
     }
-    public String getKodePembayaran()
-    {
+
+    public String getKodePembayaran() {
         return this.kodePembayaran;
     }
-    public String getKodeBooking()
-    {
+
+    public String getKodeBooking() {
         return this.kodeBooking;
     }
-    public String getKodeKereta()
-    {
+
+    public String getKodeKereta() {
         return this.kodeKereta;
     }
-    public Date getTanggalPembayaran()
-    {
-        return this.tanggalPembayaran;
+
+    public Date getTanggalPembayaran() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String strDate = formatter.format(this.tanggalPembayaran);
+        try {
+            return formatter.parse(strDate);
+        } catch (ParseException e) {
+          
+        }
+        return null;
     }
     public Double getTotalPembayaran()
     {
